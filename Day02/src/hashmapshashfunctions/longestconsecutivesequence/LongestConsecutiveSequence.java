8beebd7dd72a3d00d9c425ea3876b34b48cd4c86
aditya12696 <100424719+aditya12696@users.noreply.github.com>
2025-01-29 13:00:56 +0530

    longest consecutive sequence

---
+++ Day02/src/hashmapshashfunctions/longestconsecutivesequence/LongestConsecutiveSequence.java
@@ -0,0 +1,38 @@
+package hashmapshashfunctions.longestconsecutivesequence;
+import java.util.*;
+
+class LongestConsecutiveSequence {
+
+    static int findLongestConsecutive(int[] nums) {
+        if (nums.length == 0) return 0;
+
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        int maxLength = 0;
+
+        for (int num : set) {
+            // Start only if it's the beginning of a sequence
+            if (!set.contains(num - 1)) {
+                int currentNum = num;
+                int count = 1;
+
+                while (set.contains(currentNum + 1)) {
+                    currentNum++;
+                    count++;
+                }
+
+                maxLength = Math.max(maxLength, count);
+            }
+        }
+
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {100, 4, 200, 1, 3, 2};
+        System.out.println("Longest Consecutive Sequence Length: " + findLongestConsecutive(nums));
+    }
+}
